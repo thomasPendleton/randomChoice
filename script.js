403839dd6ab83f666a5441c661485ed0df4d0eda
Thomas Pendleton <thomas6576@yahoo.com>
2022-02-01 21:52:42 -0800

    finished project

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-const input = document.getElementById('input')
+const tagsEl = document.getElementById('tags')
+const textArea = document.getElementById('textarea')
+
+textArea.focus()
+
+textArea.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 20)
+    randomSelect()
+  }
+})
+
+function createTags(value) {
+  const tags = value
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((item) => item.trim())
+  tagsEl.innerHTML = ''
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span')
+    tagEl.classList.add('tag')
+    tagEl.innerText = tag
+    tagsEl.appendChild(tagEl)
+  })
+}
+
+function randomSelect() {
+  const times = 30
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    highlightTag(randomTag)
+    setTimeout(() => {
+      unHighlightTag(randomTag)
+    }, 100)
+  }, 100)
+  setTimeout(() => {
+    clearInterval(interval)
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
+}
